07e9c57c9cf83675d34c8587915818c04000c6e5
Evan <eastene@users.noreply.github.com>
2019-09-09 11:55:54 -0600

    Add files via upload

---
+++ Dimension Reduction/autoencoder.R
@@ -0,0 +1,95 @@
+library(keras)
+
+# Installation parameters
+use_gpu <- FALSE
+
+# Model hyperparameters
+
+if (use_gpu){
+  install_keras(tensorflow = "gpu")
+} else {
+  # install_keras(tensorflow = "default")
+}
+
+autoencoder <- function(df, layer_sizes = c(128, 64, 16), 
+                        pretrain_epochs = 100, finetune_epochs = 200,
+                        pretrain_lr = 0.01, finetune_lr = 0.00001,
+                        batch_size = 100, validation_split = 0.2){
+  # Define model, freeze all but end layers
+  model <- keras_model_sequential()
+  lrelu <- layer_activation_leaky_relu()
+  # Define encoder layers
+  model %>% layer_dense(units = layer_sizes[1], 
+                        activation = lrelu, input_shape = ncol(df),
+                        name = sprintf("Encoder_%d", 1))
+  if (length(layer_sizes) > 1){
+    encoder_no <- 2
+    for (size in layer_sizes[(2:length(layer_sizes))]){
+      model %>% layer_dense(units = size, activation = lrelu, 
+                            name = sprintf("Encoder_%d", encoder_no))
+      encoder_no <- encoder_no + 1
+    }
+    # Define decoder layers
+    for (size in rev(layer_sizes[(1:length(layer_sizes) - 1)])){
+      decoder_no <- length(layer_sizes)
+      model %>% layer_dense(units = size, activation = "linear", 
+                            name = sprintf("Decoder_%d", decoder_no))
+      decoder_no <- decoder_no - 1
+    }
+  }
+  model %>% layer_dense(units = ncol(df), activation = "linear",
+                        name = sprintf("Decoder_%d", 1))
+  
+  summary(model)
+  
+  # Compile model with MSE loss
+  model %>% compile(
+    loss = "mse",
+    optimizer = optimizer_adam(lr = pretrain_lr),
+    metrics = "mse"
+  )
+  
+  X = as.matrix(df)
+  
+  # Pretrain model layers
+  for (i in seq_along(layer_sizes)) {
+    print(sprintf("TRAINING LAYER %d", i))
+    freeze_weights(model, from = "Encoder_1", to = "Decoder_1")
+    unfreeze_weights(get_layer(model, name = sprintf("Encoder_%d", i)))
+    unfreeze_weights(get_layer(model, name = sprintf("Decoder_%d", i)))
+    
+    model %>% compile(
+      loss = "mse",
+      optimizer = optimizer_adam(lr = pretrain_lr),
+      metrics = "mse"
+    )
+    
+    history <- model %>% fit(
+      X, X, 
+      epochs = pretrain_epochs, batch_size = batch_size, 
+      validation_split = validation_split
+    )
+    
+    plot(history)
+  }
+  
+  # Finetune entire model
+  print("TRAINING ALL LAYERS IN COMBINATION ")
+  unfreeze_weights(model, from = "Encoder_1", to = "Decoder_1")
+  model %>% compile(
+    loss = "mse",
+    optimizer = optimizer_adam(lr = pretrain_lr),
+    metrics = "mse"
+  )
+  
+  history <- model %>% fit(
+    X, X, 
+    epochs = pretrain_epochs, batch_size = batch_size, 
+    validation_split = validation_split
+  )
+  
+  plot(history)
+}
+
+test_df <- read.table("filtered_met2_N-49_2019-06-06.txt", header=TRUE, row.names = "sid")
+autoencoder(test_df, c(10, 2))
