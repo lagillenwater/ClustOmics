39254e347dcab85812d05aede864e0d93b6a76c6
Evan <eastene@users.noreply.github.com>
2020-03-11 18:19:44 -0600

    Add files via upload

---
+++ R/feature_selection.R
@@ -0,0 +1,7 @@
+library(reticulate)
+
+source_python("../inst/SVMRFE.py")
+
+SVMRFE <- function(df, labels, save.ranks=T, annotate=F, plot.cv.scores=F){
+    return(SVMRFE_PY(df, labels, save_full_ranks=save.ranks, annotate=annotate, plot_cv_score=plot.cv.scores))
+}
