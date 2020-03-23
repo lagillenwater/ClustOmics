22fda297e6bc7f0b4b2e95638a062f9e971d5815
lagillenwater <36578655+lagillenwater@users.noreply.github.com>
2020-03-23 15:47:08 -0600

    Update feature_selection.R

--- R/feature_selection.R
+++ R/feature_selection.R
@@ -1,6 +1,6 @@
 library(reticulate)
 
-source_python("../inst/SVMRFE.py")
+source_python("./inst/SVMRFE.py")
 
 SVMRFE <- function(df, labels, save.ranks=T, annotate=F, plot.cv.scores=F){
     return(SVMRFE_PY(df, labels, save_full_ranks=save.ranks, annotate=annotate, plot_cv_score=plot.cv.scores))
